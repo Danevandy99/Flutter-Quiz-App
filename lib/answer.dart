fafbbd823ef6bc29ea566f115266b982af355e07
Dane Vanderbilt <danevandy@hotmail.com>
2021-08-23 10:36:36 -0500

    End of module

--- lib/answer.dart
+++ lib/answer.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
-  final Function selectHandler;
+  final VoidCallback selectHandler;
   final String answerText;
 
   Answer(this.selectHandler, String this.answerText);
@@ -10,9 +10,15 @@ class Answer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      child: RaisedButton(
-        color: Colors.blue,
-        textColor: Colors.white,
+      child: ElevatedButton(
+        // style: ButtonStyle(
+        //   backgroundColor: MaterialStateProperty.all(Colors.blue),
+        //   foregroundColor: MaterialStateProperty.all(Colors.white)
+        // ),
+        style: ElevatedButton.styleFrom(
+          primary: Colors.orange,
+          onPrimary: Colors.white,
+        ),
         child: Text(answerText),
         onPressed: selectHandler,
       ),
